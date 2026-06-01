3fc074f9adb8442aab4cd8a04231ce8f6af8f319
krishnashahu214 <krishnashahu214@gmail.com>
2026-06-01 23:24:44 +0530

    Prepare deployment

--- models/text_emotion.py
+++ models/text_emotion.py
@@ -1,4 +1,14 @@
-from transformers import pipeline
+# from transformers import pipeline
+
+# # classifier = pipeline(
+# #     "text-classification",
+# #     model="j-hartmann/emotion-english-distilroberta-base"
+# # )
+
+# # def get_text_emotion(text):
+# #     result = classifier(text)
+# #     return result[0]['label']
+
 
 # classifier = pipeline(
 #     "text-classification",
@@ -7,16 +17,34 @@ from transformers import pipeline
 
 # def get_text_emotion(text):
 #     result = classifier(text)
-#     return result[0]['label']
+#     label = result[0]['label']
+#     confidence = result[0]['score']
+    
+#     return label, confidence
 
 
-classifier = pipeline(
-    "text-classification",
-    model="j-hartmann/emotion-english-distilroberta-base"
-)
+
+
+from transformers import pipeline
+
+classifier = None
+
+def get_classifier():
+    global classifier
+
+    if classifier is None:
+        classifier = pipeline(
+            "text-classification",
+            model="j-hartmann/emotion-english-distilroberta-base"
+        )
+
+    return classifier
+
 
 def get_text_emotion(text):
-    result = classifier(text)
+    clf = get_classifier()
+
+    result = clf(text)
     label = result[0]['label']
     confidence = result[0]['score']
     
